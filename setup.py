1a210efd8823905eb6d7f99ca4e57a684e114a64
baoilleach <baoilleach@4a1743fc-e749-0410-b876-edcebe66cb5e>
2008-05-06 20:53:56 +0000

    git-svn-id: http://cinfony.googlecode.com/svn/trunk@33 4a1743fc-e749-0410-b876-edcebe66cb5e

---
+++ setup.py
@@ -0,0 +1,42 @@
+"""cinfony: a common API to several cheminformatics toolkits
+
+cinfony is a Python library that provides a common API to several
+open source cheminformatics toolkits.
+"""
+
+doclines = __doc__.split("\n")
+
+# Chosen from http://www.python.org/pypi?:action=list_classifiers
+classifiers = """\
+Development Status :: 4 - Beta
+Environment :: Console
+Intended Audience :: Science/Research
+Intended Audience :: Developers
+License :: OSI Approved :: BSD License
+Natural Language :: English
+Operating System :: OS Independent
+Programming Language :: Python
+Topic :: Scientific/Engineering :: Chemistry
+Topic :: Software Development :: Libraries :: Python Modules
+"""
+
+from distutils.core import setup
+setup(
+    name = "cinfony",
+    version = "0.1",
+    url = "http://code.googlecode.com/p/cinfony",
+    author = "Noel O'Boyle",
+    author_email = "baoilleach@gmail.com",
+    maintainer = "Noel O'Boyle",
+    maintainer_email = "baoilleach@gmail.com",
+    license = "BSD",
+    description = doclines[0],
+    long_description = "\n".join(doclines[2:]),      
+    classifiers = filter(None, classifiers.split("\n")),
+    platforms = ["Any."],
+#    scripts = ["src/scripts/ccget", "src/scripts/cda"],
+#    package_dir = {'':'cinfony'},
+    packages = ['cinfony'],
+#    packages = cclib_packages,
+    )
+
