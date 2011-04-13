cd77418f60570f67e068c5848cfd6e41699ea179
baoilleach <baoilleach@gmail.com>
2011-04-13 19:34:38 +0100

    Fix minor error in previous commits

--- cinfony/cdkjpype.py
+++ cinfony/cdkjpype.py
@@ -20,8 +20,9 @@ import StringIO
 
 from jpype import *
 
-_jvm = os.environ['JPYPE_JVM']
-_cp = os.environ['CLASSPATH']
+if not isJVMStarted():
+    _jvm = os.environ['JPYPE_JVM']
+    _cp = os.environ['CLASSPATH']
 startJVM(_jvm, "-Djava.class.path=" + _cp)
 
 try:
